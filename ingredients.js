c37d112e6329d120820b889771b4e7f4901b0419
David Morgan <david.alexandermorgan@gmail.com>
2018-07-04 22:07:13 +0000

    looping assignment

---
+++ ingredients.js
@@ -0,0 +1,16 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+var counter = 0;
+
+while (counter < ingredients.length){
+  console.log(ingredients[counter]);
+  counter ++
+}
+
+for (i = 0; i < ingredients.length; i++){
+  console.log(ingredients[i]);
+}
+
+for (i = ingredients.length - 1; i >= 0; i--){
+  console.log(ingredients[i]);
+}
